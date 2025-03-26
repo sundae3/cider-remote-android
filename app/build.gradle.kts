38b7b057ab387bf8979e065985daf54ff18ca1d9
sundae3 <lamdatester999@gmail.com>
2025-03-26 10:26:45 +0530

    bumped version for new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 25
         //noinspection EditedTargetSdkVersion
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
